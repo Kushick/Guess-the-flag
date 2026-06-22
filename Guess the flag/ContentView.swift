b35d511ebcf3544eb815bceb17217dc02f20ae5c
Kushick <cbkushick@gmail.com>
2026-06-22 13:27:49 +0600

    Initial Commit

---
+++ Guess the flag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guess the flag
+//
+//  Created by Kushick Chakraborty on 22/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by Kushick Chakraborty on 22/6/26.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
